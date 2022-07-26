e67775e7937af49c879dbada2e4960d9a1d6b3e6
David Heyman <dave@axismaps.com>
2022-07-26 12:13:55 +0100

    Added story for Atlas

--- .storybook/custom-decorators/withChakraUi.js
+++ .storybook/custom-decorators/withChakraUi.js
@@ -1,6 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react'
 
 import theme from '../../src/theme'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
 export const withChakraUi = (Story) => (
   <ChakraProvider resetCSS theme={theme}>
---
+++ src/components/Atlas/stories.tsx
@@ -0,0 +1,15 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { Box } from '@chakra-ui/react'
+
+import Atlas from '.'
+
+export default {
+  title: 'Atlas',
+  component: Atlas
+} as Meta
+
+export const Default: Story = () => (
+  <Box h="calc(100vh - 50px)" w="full">
+    <Atlas />
+  </Box>
+)
